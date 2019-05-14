1d481b402497ee30d93345a3fda48b269c8318ad
Kamyab Zareh <kamyab.zareh@gmail.com>
2019-05-14 11:35:52 +0430

    add offer script

--- script.js
+++ script.js
@@ -3,15 +3,43 @@
  * @param {com.khazandegan.library.Trade} trade - the trade to be processed
  * @transaction
  */
-async function Trade(trade) {  // eslint-disable-line no-unused-vars
-    const oldOwner = trade.book.owner;
-    trade.book.owner = trade.orderer;
-    const assetRegistry = await getAssetRegistry('com.khazandegan.library.Book');
-    await assetRegistry.update(trade.book);
-    let event = getFactory().newEvent('com.khazandegan.library', 'PlaceTradeEvent');
-    event.orderID = trade.orderID;
-    event.book = trade.book;
+async function Trade(trade) {
+  // eslint-disable-line no-unused-vars
+  const oldOwner = trade.book.owner;
+  trade.book.owner = trade.orderer;
+  const assetRegistry = await getAssetRegistry("com.khazandegan.library.Book");
+  await assetRegistry.update(trade.book);
+  let event = getFactory().newEvent(
+    "com.khazandegan.library",
+    "PlaceTradeEvent"
+  );
+  event.orderID = trade.orderID;
+  event.book = trade.book;
+  event.oldOwner = oldOwner;
+  event.newOwner = trade.orderer;
+  emit(event);
+}
+
+async function Offer(offer, isAccepted) {
+  // eslint-disable-line no-unused-vars
+  if (isAccepted) {
+    const oldOwner = offer.book.owner;
+    offer.book.owner = offer.orderer;
+    oldOwner.wallet.totalWallet += offer.price;
+    offer.orderer.wallet.totalWallet -= offer.price;
+    offer.status = OfferStatus.DONE;
+    const assetRegistry = await getAssetRegistry(
+      "com.khazandegan.library.Book"
+    );
+    await assetRegistry.update(offer.book);
+    let event = getFactory().newEvent(
+      "com.khazandegan.library",
+      "PlaceOfferEvent"
+    );
+    event.offerID = offer.offerID;
+    event.book = offer.book;
     event.oldOwner = oldOwner;
-    event.newOwner = trade.orderer;
+    event.newOwner = offer.orderer;
     emit(event);
+  }
 }
